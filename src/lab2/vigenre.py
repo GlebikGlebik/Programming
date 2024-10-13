b4251ee021601d6cd02d93e7f88b76eda21a1929
Gleb <Glebsd22@gmail.com>
2024-10-14 00:31:51 +0300

    vigenre task done

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -8,8 +8,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = [x for x in plaintext]
+    keyword = keyword.upper()
+    keyword = [x for x in keyword]
+
+    if len(keyword) > len(ciphertext):
+        keyword = keyword[: len(ciphertext)]
+
+    while len(keyword) < len(ciphertext):
+        keyword += keyword
+        if len(keyword) > len(ciphertext):
+            keyword = keyword[: len(ciphertext)]
+
+    for i in range(len(ciphertext)):
+        if  65 <= ord(ciphertext[i]) <= 90 - (ord(keyword[i]) - 65):
+            ciphertext[i] = chr(ord(ciphertext[i]) + (ord(keyword[i]) - 65))
+        elif 90 - (ord(keyword[i]) - 65) <= ord(ciphertext[i]) <= 90:
+            ciphertext[i] = chr(65 + abs(90 - (ord(ciphertext[i]) + (ord(keyword[i]) - 65))) - 1)
+        if  97 <= ord(ciphertext[i]) <= 122 - (ord(keyword[i]) - 65):
+            ciphertext[i] = chr(ord(ciphertext[i]) + (ord(keyword[i]) - 65))
+        elif 122 - (ord(keyword[i]) - 65) <= ord(ciphertext[i]) <= 122:
+            ciphertext[i] = chr(97 + abs(122 - (ord(ciphertext[i]) + (ord(keyword[i]) - 65))) - 1)
+
+    ciphertext = "".join(ciphertext)
     return ciphertext
 
 
@@ -23,6 +44,27 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = [x for x in ciphertext]
+    keyword = keyword.upper()
+    keyword = [x for x in keyword]
+
+    if len(keyword) > len(ciphertext):
+        keyword = keyword[: len(ciphertext)]
+
+    while len(keyword) < len(ciphertext):
+        keyword += keyword
+        if len(keyword) > len(ciphertext):
+            keyword = keyword[: len(ciphertext)]
+
+    for i in range(len(plaintext)):
+        if 65 + (ord(keyword[i]) - 65) <= ord(plaintext[i]) <= 90:
+            plaintext[i] = chr(ord(plaintext[i]) - (ord(keyword[i]) - 65))
+        elif 65 <= ord(plaintext[i]) <= 65 + (ord(keyword[i]) - 65):
+            plaintext[i] = chr(90 - abs(65 - (ord(plaintext[i]) - (ord(keyword[i]) - 65))) + 1)
+        if 97 + (ord(keyword[i]) - 65) <= ord(plaintext[i]) <= 122:
+            plaintext[i] = chr(ord(plaintext[i]) - (ord(keyword[i]) - 65))
+        elif 97 <= ord(plaintext[i]) <= 97 + (ord(keyword[i]) - 65):
+            plaintext[i] = chr(122 - abs(97 - (ord(plaintext[i]) - (ord(keyword[i]) - 65))) + 1)
+
+    plaintext = "".join(plaintext)
     return plaintext
