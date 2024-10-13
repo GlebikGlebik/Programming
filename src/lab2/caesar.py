27da3f53e00458e40f9f25101493607771ca2bdc
Gleb <Glebsd22@gmail.com>
2024-10-13 23:55:50 +0300

    caesar task done

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -10,8 +10,18 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = [x for x in plaintext]
+    for i in range(len(ciphertext)):
+        if  65 <= ord(ciphertext[i]) <= 90 - shift:
+            ciphertext[i] = chr(ord(ciphertext[i]) + shift)
+        elif 90 - shift <= ord(ciphertext[i]) <= 90:
+            ciphertext[i] = chr(65 + abs(90 - (ord(ciphertext[i]) + shift)) - 1)
+        if  97 <= ord(ciphertext[i]) <= 122 - shift:
+            ciphertext[i] = chr(ord(ciphertext[i]) + shift)
+        elif 122 - shift <= ord(ciphertext[i]) <= 122:
+            ciphertext[i] = chr(97 + abs(122 - (ord(ciphertext[i]) + shift)) - 1)
+
+    ciphertext = "".join(ciphertext)
     return ciphertext
 
 
@@ -27,6 +37,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+    plaintext = [x for x in ciphertext]
+    for i in range(len(plaintext)):
+        if 65 + shift <= ord(plaintext[i]) <= 90:
+            plaintext[i] = chr(ord(plaintext[i]) - shift)
+        elif 65 <= ord(plaintext[i]) <= 65 + shift:
+            plaintext[i] = chr(90 - abs(65 - (ord(plaintext[i]) - shift)) + 1)
+        if 97 + shift <= ord(plaintext[i]) <= 122:
+            plaintext[i] = chr(ord(plaintext[i]) - shift)
+        elif 97 <= ord(plaintext[i]) <= 97 + shift:
+            plaintext[i] = chr(122 - abs(97 - (ord(plaintext[i]) - shift)) + 1)
+
+
+    plaintext = "".join(plaintext)
     return plaintext
