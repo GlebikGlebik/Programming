bdf2cd1f2e7e725e1ba954a1448591069ed0a80e
Gleb <Glebsd22@gmail.com>
2024-10-23 17:18:03 +0300

    function group in sudoku.py done

--- src/lab3/sudoku.py
+++ src/lab3/sudoku.py
@@ -40,8 +40,35 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     [[1, 2], [3, 4]]
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    >>> group([1,2], 1)
+    [[1], [2]]
+    >>> group([2], 1)
+    [[2]]
+    >>> group([], 2)
+    Traceback (most recent call last):
+        ...
+    ValueError: Список не может быть пустым
+    >>> group([1, 2, 3], 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Данный список невозможно поделить на подписки с таким количеством элементов
+    >>> group([1, 2, 3], 0)
+    Traceback (most recent call last):
+        ...
+    ValueError: Размер группы не может быть нулевым или отрицательным
     """
-    pass
+    b = []
+    if n <= 0:
+        raise ValueError("Размер группы не может быть нулевым или отрицательным")
+    if len(values) == 0:
+        raise ValueError("Список не может быть пустым")
+    if len(values) % n != 0:
+        raise ValueError("Данный список невозможно поделить на подписки с таким количеством элементов")
+    for i in range(0, len(values), n):
+        b.append(values[i : i + n])
+    return b
+
+
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
