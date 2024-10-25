9b1198847316dc8eeabd7d34bd35527b945f1631
Gleb <Glebsd22@gmail.com>
2024-10-25 23:59:40 +0300

    unittests for get_block function from sudoku done

---
+++ tests/lab3/test_get_block.py
@@ -0,0 +1,18 @@
+import unittest
+from src.lab3.sudoku import *
+
+
+class Get_blockTestCase(unittest.TestCase):
+
+    def test_one(self):
+        grid = read_sudoku('puzzle1.txt')
+        self.assertEqual(get_block(grid, (0, 1)), ['5', '3', '.', '6', '.', '.', '.', '9', '8'])
+
+    def test_two(self):
+        grid = read_sudoku('puzzle2.txt')
+        self.assertEqual(get_block(grid, (4, 7)), ['.', '.', '.', '.', '.', '.', '.', '6', '.'])
+
+    def test_three(self):
+        grid = read_sudoku('puzzle3.txt')
+        self.assertEqual(get_block(grid, (8, 8)), ['.', '9', '.', '.', '.', '.', '.', '.', '5'])
+
