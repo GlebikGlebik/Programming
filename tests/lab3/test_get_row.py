78682305e00c4d556f5cd90bcbb3f7342303c437
Gleb <Glebsd22@gmail.com>
2024-10-25 22:57:26 +0300

    unittests for get_row function from sudoku done

---
+++ tests/lab3/test_get_row.py
@@ -0,0 +1,17 @@
+import unittest
+from src.lab3.sudoku import *
+
+
+class Get_rowTestCase(unittest.TestCase):
+
+    def test_one(self):
+        self.assertEqual(get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0)), ['1', '2', '.'])
+
+    def test_two(self):
+        self.assertEqual(
+            get_row([['1', '2', '3', '4', '5'], ['6', '7', '8', '.', '.'], ['.', '.', '9', '.', '10']], (2, 0)),
+            ['.', '.', '9', '.', '10'])
+
+    def test_three(self):
+        self.assertEqual(get_row([['.', '.', '9', '.', '1', '2', '3'], ['.', '.', '9', '.', '1', '2', '3'],
+                                  ['4', '5', '6', '7', '8', '9', '10']], (1, 0)), ['.', '.', '9', '.', '1', '2', '3'])
