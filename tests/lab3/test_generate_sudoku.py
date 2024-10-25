9d998f4d15f8f1507d674c2eb6b492832f81d092
Gleb <Glebsd22@gmail.com>
2024-10-26 00:20:12 +0300

    unittests for generate_sudoku function from sudoku done

---
+++ tests/lab3/test_generate_sudoku.py
@@ -0,0 +1,32 @@
+import unittest
+from src.lab3.sudoku import *
+
+
+class generate_sudokuTestCase(unittest.TestCase):
+
+    def test_count_dots_one(self):
+        grid = generate_sudoku(40)
+        self.assertEqual(sum(1 for row in grid for e in row if e == '.'), 41)
+
+    def test_count_dots_two(self):
+        grid = generate_sudoku(0)
+        self.assertEqual(sum(1 for row in grid for e in row if e == '.'), 81)
+
+    def test_count_dots_three(self):
+        grid = generate_sudoku(81)
+        self.assertEqual(sum(1 for row in grid for e in row if e == '.'), 0)
+
+    def test_puzzle1(self):
+        grid = read_sudoku('puzzle1.txt')
+        solution = solve(grid)
+        self.assertEqual(check_solution(solution), True)
+
+    def test_puzzle2(self):
+        grid = read_sudoku('puzzle2.txt')
+        solution = solve(grid)
+        self.assertEqual(check_solution(solution), True)
+
+    def test_puzzle3(self):
+        grid = read_sudoku('puzzle3.txt')
+        solution = solve(grid)
+        self.assertEqual(check_solution(solution), True)
