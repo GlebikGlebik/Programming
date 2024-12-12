c356bb54f707418df92bacd5b3ee48204016e908
Gleb <Glebsd22@gmail.com>
2024-12-12 23:03:35 +0300

    updated

--- tests/lab3/test_task1/test_MovieRecommendation.py
+++ tests/lab3/test_task1/test_MovieRecommendation.py
@@ -1,30 +1,54 @@
 import unittest
+import tempfile
+import os
 from src.lab3.task1.MovieRecommendation import *
 
 class MovieRecommendationTestCase(unittest.TestCase):
+    def setUp(self):
+        # Создаем временные файлы для фильмов и пользователей
+        self.films_data = "1,Дюна\n2,Мстители: Финал\n3,Хатико\n4,Унесенные призраками\n"
+        self.users_data = "1,2,3\n2,3,4\n3,4,5\n4,5,6\n"
+
+        self.films_file = tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8')
+        self.users_file = tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8')
+
+        self.films_file.write(self.films_data)
+        self.films_file.close()
+
+        self.users_file.write(self.users_data)
+        self.users_file.close()
+
+        # Инициализируем экземпляр класса
+        self.movie_recommendation = MovieRecommendation()
+        self.movie_recommendation.films_file = self.films_file.name
+        self.movie_recommendation.users_file = self.users_file.name
+
+    def tearDown(self):
+        # Удаляем временные файлы после тестов
+        os.remove(self.films_file.name)
+        os.remove(self.users_file.name)
+
     def test_one(self):
         """Тестирует случай, когда совпадает ровно половина фильмов."""
-        main_user_history = ['1','2']
-        self.assertEqual(MovieRecommendation.main(main_user_history), "Дюна")
+        self.movie_recommendation.main_user_history = ['1', '2']
+        self.movie_recommendation.result()
+        self.assertEqual(self.movie_recommendation.film_name, "Дюна")
 
     def test_two(self):
-        main_user_history = ['2', '3']
-        self.assertEqual(MovieRecommendation.main(main_user_history), "Мстители: Финал")
+        self.movie_recommendation.main_user_history = ['2', '3']
+        self.movie_recommendation.result()
+        self.assertEqual(self.movie_recommendation.film_name, "Мстители: Финал")
 
     def test_three(self):
-        main_user_history = ['2', '4']
-        self.assertEqual(MovieRecommendation.main(main_user_history), "Дюна")
+        self.movie_recommendation.main_user_history = ['2', '4']
+        self.movie_recommendation.result()
+        self.assertEqual(self.movie_recommendation.film_name, "Дюна")
 
     def test_no_recommendation(self):
-        """Тестирует случай, когда нет рекомендаций так как пользователь уже посмотрел все фильмы"""
-        main_user_history = [1, 2, 3, 4]
-        self.assertEqual(MovieRecommendation.main(main_user_history), "Подходящих рекомендаций для пользователя нет")
-
-    def test_empty_main_user_movies(self):
-        """Тестирует случай, когда пользователь не смотрел ни одного фильма."""
-        main_user_history = list()
-        self.assertIn(MovieRecommendation.main(main_user_history), ['Мстители: Финал', 'Хатико', 'Дюна', 'Унесенные призраками'])
-
+        """Тестирует случай, когда нет рекомендаций так как пользователь уже посмотрел все фильмы."""
+        self.movie_recommendation.main_user_history = ['1', '2', '3', '4']
+        self.movie_recommendation.result()
+        self.assertEqual(self.movie_recommendation.film_name, "Подходящих рекомендаций для пользователя нет")
 
 if __name__ == '__main__':
     unittest.main()
